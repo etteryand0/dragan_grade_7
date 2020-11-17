cd127b10360bc3eae4be592942f0035c83ad4f58
etteryand0 <etteryand@gmail.com>
2020-11-17 17:59:23 +0900

    shitty hamburger

--- src/components/jsx/navbar/hamburger/Hamburger.js
+++ src/components/jsx/navbar/hamburger/Hamburger.js
@@ -5,7 +5,10 @@ class Hamburger extends React.Component {
   render() {
     return (
       <div className={styles.menu_toggle}>
-        <span></span>
+        <input type="checkbox" />
+        <span className={[styles.hamburger, styles.stick_1].join(" ")}></span>
+        <span className={[styles.hamburger, styles.stick_2].join(" ")}></span>
+        <span className={[styles.hamburger, styles.stick_3].join(" ")}></span>
       </div>
     );
   }
